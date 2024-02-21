cbf555d0a18af7d624b5fe76fc617c4389a479d4
Tofeeq <tofeeq09@gmail.com>
2024-02-21 20:54:10 +0000

    enhancement: add Links to navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,19 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 // import PropTypes from "prop-types";
 import Container from "./Container";
 
 const NavBar = () => {
   return (
     <Container>
-      <nav></nav>
+      <nav>
+        <Link to="/">
+          <div className="name">Bookshelf</div>
+        </Link>
+        <div>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link>
+        </div>
+      </nav>
     </Container>
   );
 };
